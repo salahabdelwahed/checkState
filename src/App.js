d247d67c04eed4066a77d448cc08d7e6d226df74
Your Name <Your email>
2024-01-27 22:25:50 +0100

    last Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+state={
+  person:{
+    fullname: 'Messi',
+    bio: 'GOAT',
+    imgSrc:'https://static.ffx.io/images/$zoom_0.242%2C$multiply_0.7725%2C$ratio_1.5%2C$width_756%2C$x_0%2C$y_0/t_crop_custom/q_86%2Cf_auto/a0e24e8dfe9e0e93d1a6228efdd4543222819005',
+    profession:'football player'
+  },
+  show: false,
+}
+toggleShow=() =>{
+  this.setState((prevstate) =>({
+    person: !prevstate.schow
+  }))
 }
 
-export default App;
+  render() {
+    const {fullname,bio,imgSrc,profession} =this.state.person ; 
+    const show = this.props.show ;
+    
+    return (
+      <div> 
+        <button onClick={this.toggleShow}>show</button>
+        <>
+        {""}
+        <h1>{fullname}</h1>
+        <h6>{bio}</h6>
+        <img src={imgSrc} alt="" />
+        <p>{profession}</p>
+        </>
+      </div>
+    )
+  }
+}
+export default App
