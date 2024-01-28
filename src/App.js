f29b58e35f5600a66ecde272e81ef7ec831933d7
Your Name <Your email>
2024-01-28 10:27:02 +0100

    last update time

--- src/App.js
+++ src/App.js
@@ -9,14 +9,24 @@ state={
     imgSrc:'https://static.ffx.io/images/$zoom_0.242%2C$multiply_0.7725%2C$ratio_1.5%2C$width_756%2C$x_0%2C$y_0/t_crop_custom/q_86%2Cf_auto/a0e24e8dfe9e0e93d1a6228efdd4543222819005',
     profession:'football player'
   },time:0,
-  show: false,
-
+  show: true,
 }
-toggleShow=() => {
+
+componentDidMount() {
+  this.interval = setInterval(() =>{
+    this.setState((prevstate) =>({
+      timer: prevstate.timer+1 ,
+    }));
+  },1000);
+  }
+
+
+
+toggleShow = () => {
   this.setState((prevstate) =>({
     show: !prevstate.show,
   }));
-};
+}
 
   render() {
     const {fullname,bio,imgSrc,profession} =this.state.person ; 
@@ -37,8 +47,8 @@ toggleShow=() => {
         <p>{profession}</p>{""}) 
         </> :(
           <>
-          {/* h1 hgfjd; {this.state.timer} */}
-          <h1>nothing</h1>
+        
+          <h1>this for timer : {this.state.timer}</h1>
           </>
         ))
       </div>
